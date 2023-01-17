b6b8e77a6d26e7487969b5ca6db003e89d54831f
manishcoforge <manish.26.kumar@coforge.com>
2023-01-17 18:55:43 +0530

    Updates

---
+++ precache-manifest.c4c76295c8b0b14bd9560ea517b009d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e65c9ae93c19d20eda9",
+    "url": "/matclaims/static/css/main.eb7b13f9.chunk.css"
+  },
+  {
+    "revision": "4e65c9ae93c19d20eda9",
+    "url": "/matclaims/static/js/main.4e65c9ae.chunk.js"
+  },
+  {
+    "revision": "17b8b06a490f0a45ea36",
+    "url": "/matclaims/static/js/1.17b8b06a.chunk.js"
+  },
+  {
+    "revision": "79d5ab0d5a1446babc75",
+    "url": "/matclaims/static/js/runtime~main.79d5ab0d.js"
+  },
+  {
+    "revision": "07eb1a78ab971905cf01860eae657df7",
+    "url": "/matclaims/static/media/footer.07eb1a78.png"
+  },
+  {
+    "revision": "9707a71a4b6b6bef00ace99a307ae5bb",
+    "url": "/matclaims/static/media/barclayCard.9707a71a.png"
+  },
+  {
+    "revision": "3139583580bfc2a34b31f5f1d2b39d3d",
+    "url": "/matclaims/index.html"
+  }
+];
